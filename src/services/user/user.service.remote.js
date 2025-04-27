c9683249c547b10707e96fbccc9b41e90f5093dd
supaGirl <kerenvasserman.bm@gmail.com>
2025-04-27 20:26:42 +0300

    Add user service and store

---
+++ src/services/user/user.service.remote.js
@@ -0,0 +1,63 @@
+import {httpService} from '../http.service.js'
+
+const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
+
+export const userService = {
+  login,
+  logout,
+  signup,
+  getLoggedinUser,
+  query,
+  getById,
+  remove,
+  update,
+}
+
+function query() {
+  return httpService.get('user')
+}
+
+async function getById(userId) {
+  const user = await httpService.get(`user/${userId}`)
+  return user
+}
+
+function remove(userId) {
+  return httpService.delete(`user/${userId}`)
+}
+
+async function update({_id}) {
+  const user = await httpService.put(`user/${_id}`)
+
+  // When admin updates other user's details, do not update loggedinUser
+  const loggedinUser = getLoggedinUser() // Might not work because its defined in the main service???
+  if (loggedinUser._id === user._id) _saveLocalUser(user)
+
+  return user
+}
+
+async function login(userCred) {
+  const user = await httpService.post('auth/login', userCred)
+  if (user) {
+    return _saveLocalUser(user)
+  }
+}
+async function signup(userCred) {
+  if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
+  const user = await httpService.post('auth/signup', userCred)
+  return _saveLocalUser(user)
+}
+async function logout() {
+  sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+  return await httpService.post('auth/logout')
+}
+
+function getLoggedinUser() {
+  return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
+}
+
+function _saveLocalUser(user) {
+  user = {_id: user._id, fullname: user.fullname, imgUrl: user.imgUrl, isAdmin: user.isAdmin}
+  sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
+  return user
+}
